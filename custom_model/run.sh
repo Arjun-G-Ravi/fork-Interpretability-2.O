da790f17300bd95e973b230f746178b500f81b92
Skylinez-Asylum <a.devanand007@gmail.com>
2024-10-08 18:32:09 +0530

    New model Code

---
+++ custom_model/run.sh
@@ -0,0 +1,12 @@
+torchrun --standalone --nproc_per_node=1 train_gpt2.py \
+    --input_bin "data/fineweb10B/fineweb_train_*.bin" \
+    --input_val_bin "data/fineweb10B/fineweb_val_*.bin" \
+    --model d12 \
+    --batch_size 64 \
+    --sequence_length 1024 \
+    --val_loss_every 128 \
+    --num_iterations 7000 \
+    --weight_decay 0.1 \
+    --learning_rate 0.0018 \
+    --warmup_iters 250 \
+    --warmdown_iters 2000
